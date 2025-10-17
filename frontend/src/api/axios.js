1db49d8137622388ca6d0d3572c1c7bd972de486
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2025-10-17 10:08:20 +0100

    Connect to Render Backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://study-buddy-2o9t.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
